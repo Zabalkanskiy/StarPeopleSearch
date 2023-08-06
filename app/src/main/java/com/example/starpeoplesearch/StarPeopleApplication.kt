46213a113ea11c2fc693e95c09a10e220c5f30e6
Zabalkanskiy <azabalkanskij@hotmail.com>
2023-08-06 10:02:49 +0300

    Add permission "Internet" in manifest add application name in manifest StarPeopleApplication.kt create StarPeopleApplication.kt

--- app/src/main/java/com/example/starpeoplesearch/StarPeopleApplication.kt
+++ app/src/main/java/com/example/starpeoplesearch/StarPeopleApplication.kt
@@ -1,6 +1,16 @@
 package com.example.starpeoplesearch
 
 import android.app.Application
+import android.content.Context
 
 class StarPeopleApplication : Application(){
+    init {
+        app = this
+    }
+
+    companion object {
+        private lateinit var app : StarPeopleApplication
+
+        fun getAppContext(): Context = app.applicationContext
+    }
 }
