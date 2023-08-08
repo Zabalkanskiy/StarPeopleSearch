9bfd18cb28a68ac3b21e65e4a9c5ec513633c101
Zabalkanskiy <azabalkanskij@hotmail.com>
2023-08-08 16:56:42 +0300

    add extrea logic for right check List<Item>

--- app/src/main/java/com/example/starpeoplesearch/recycler/RecyclerViewItem.kt
+++ app/src/main/java/com/example/starpeoplesearch/recycler/RecyclerViewItem.kt
@@ -1,11 +1,15 @@
 package com.example.starpeoplesearch.recycler
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.starpeoplesearch.R
 import com.example.starpeoplesearch.StarPeopleApplication
 import com.example.starpeoplesearch.helper.Item
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
 
 class RecyclerViewItem(var items : List<Item>) : RecyclerView.Adapter<ViewHolderItem>() {
     var filterList = mutableListOf<Item>()
@@ -26,11 +30,25 @@ class RecyclerViewItem(var items : List<Item>) : RecyclerView.Adapter<ViewHolder
         holder.name.setText(positionItem.name)
         holder.information.setText((positionItem.information))
         holder.flavoredCheckBox.setOnClickListener{view ->
-            filterList.get(position).favourite = !filterList.get(position).favourite
-            //Нужно вставить логику щелчка нужно сохранить данные или в базе или в sharedPreferences
 
+            //Нужно вставить логику щелчка нужно сохранить данные или в базе или в sharedPreferences
+            if (filterList.get(position).favourite){
+                val itemIf = filterList.get(position)
+                //???
+
+                deleteFlavoredName(context = StarPeopleApplication.getAppContext(), item = itemIf)
+                itemIf.favourite = !itemIf.favourite
+            }else{
+                val elseIf = filterList.get(position)
+                elseIf.favourite = !elseIf.favourite
+                saveFlavoredName(context = StarPeopleApplication.getAppContext(), item = elseIf)
+
+
+            }
+            //filterList.get(position).favourite = !filterList.get(position).favourite
         }
         holder.flavoredCheckBox.isChecked = filterList.get(position).favourite
+    //    notifyDataSetChanged()
 
     }
 
@@ -38,4 +56,28 @@ class RecyclerViewItem(var items : List<Item>) : RecyclerView.Adapter<ViewHolder
         filterList = newItem.toMutableList()
         notifyDataSetChanged()
     }
+
+    fun getData(): List<Item>{
+        var pferFlavored = loadFlavoredName(StarPeopleApplication.getAppContext())
+        if(pferFlavored.isNotEmpty()){
+            pferFlavored.forEach{flav ->
+                 filterList.forEach{ list ->
+                     if(list.name == flav.name){
+                        list.favourite = true
+                      }else {
+                           list.favourite = false
+                    }
+                 }
+            }
+        } else {
+            filterList.forEach({list ->
+                list.favourite = false
+            })
+        }
+        return filterList
+    }
+
+
 }
+
+
