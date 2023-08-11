47d4de086b45140a7d456afd77edd93ca9fdfc02
Zabalkanskiy <azabalkanskij@hotmail.com>
2023-08-11 11:05:43 +0300

    Add Instrumented test

--- app/src/androidTest/java/com/example/starpeoplesearch/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/starpeoplesearch/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package com.example.starpeoplesearch
 
+import android.content.Context
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.starpeoplesearch.helper.Item
+import com.example.starpeoplesearch.recycler.deleteFlavoredName
+import com.example.starpeoplesearch.recycler.loadFlavoredName
+import com.example.starpeoplesearch.recycler.saveFlavoredName
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +21,38 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    lateinit var appContext :Context
+    @Before
+    fun setUp(){
+        appContext = InstrumentationRegistry.getInstrumentation().targetContext
+    }
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.starpeoplesearch", appContext.packageName)
+    }
+
+    @Test
+    fun useSaveFlavoredandLoadFlavored() {
+        val testItem = Item(name = "TestName", information = "information", favourite = false)
+        val testList: List<Item> = listOf(testItem)
+        saveFlavoredName(appContext, testItem)
+        val list: List<Item> = loadFlavoredName(appContext)
+        val item =list.find{item -> item.name == "TestName" }
+        assertEquals(testItem, item)
+    }
+
+    @Test
+    fun useSaveFlavoredandDeleteFlavored() {
+        val testItem = Item(name = "TestName", information = "information", favourite = false)
+        val testList: List<Item> = listOf(testItem)
+        saveFlavoredName(appContext, testItem)
+        deleteFlavoredName(appContext, testItem)
+        val list: List<Item> = loadFlavoredName(appContext)
+        val item = list.find{item -> item.name == "TestName" }
+        var trueItem = false
+        if(item == null){trueItem = true}
+        assertTrue(trueItem)
     }
 }
